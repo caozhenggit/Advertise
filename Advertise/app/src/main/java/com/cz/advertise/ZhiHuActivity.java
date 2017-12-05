355203cd67ae29906a906919f944dabb630ef7a9
CaoZGit <1074401754@qq.com>
2017-12-05 22:49:03 +0800

    添加仿知乎的创意滚动广告

---
+++ Advertise/app/src/main/java/com/cz/advertise/ZhiHuActivity.java
@@ -0,0 +1,82 @@
+package com.cz.advertise;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author caozheng
+ * @date 2017/12/5
+ *
+ * describe: 仿知乎创意广告
+ */
+
+public class ZhiHuActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhihu);
+        ButterKnife.bind(this);
+
+        List<String> mockDatas = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            mockDatas.add(i + "");
+        }
+
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        mRecyclerView.setAdapter(new CommonAdapter<String>(ZhiHuActivity.this,
+                R.layout.item_ad_zhihu,
+                mockDatas) {
+            @Override
+            protected void convert(ViewHolder holder, String o, int position) {
+                if (position > 0 && position % 7 == 0) {
+                    holder.setVisible(R.id.id_tv_title, false);
+                    holder.setVisible(R.id.id_tv_desc, false);
+                    holder.setVisible(R.id.id_iv_ad, true);
+                } else {
+                    holder.setVisible(R.id.id_tv_title, true);
+                    holder.setVisible(R.id.id_tv_desc, true);
+                    holder.setVisible(R.id.id_iv_ad, false);
+                }
+            }
+        });
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int fPos = mLinearLayoutManager.findFirstVisibleItemPosition();
+                int lPos = mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
+                for (int i = fPos; i <= lPos; i++) {
+                    View view = mLinearLayoutManager.findViewByPosition(i);
+                    AdImageViewVersion1 adImageView = view.findViewById(R.id.id_iv_ad);
+                    if (adImageView.getVisibility() == View.VISIBLE) {
+                        adImageView.setDy(mLinearLayoutManager.getHeight() - view.getTop());
+                    }
+                }
+            }
+        });
+
+    }
+}
